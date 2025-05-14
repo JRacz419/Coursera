0f4a4d3b8b325c777a6addcce44f5040f9d74993
josh <jracz419@gmail.com>
2025-05-14 14:05:48 -0400

    final admin commit and screenshot

--- assessmentFeature/admin.py
+++ assessmentFeature/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
-
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
@@ -18,7 +17,6 @@ class ChoiceInline(admin.StackedInline):
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
-
 # Register your models here.
 
 
@@ -39,7 +37,6 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
-
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
